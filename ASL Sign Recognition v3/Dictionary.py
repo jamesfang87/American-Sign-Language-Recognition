37cde5d15b8b5c7761857a2c4bb401ad891a14b4
James Fang <113867753+jamesfang87@users.noreply.github.com>
2024-04-26 00:57:29 -0500

    Update Dictionary.py

--- ASL Sign Recognition v3/Dictionary.py
+++ ASL Sign Recognition v3/Dictionary.py
@@ -3,9 +3,20 @@ from Sign import Sign
 
 
 class Dictionary:
-    def __init__(self, vocab: list[Sign]):
+    def __init__(self, *sessions):
         # this is an array of all the signs that the dictionary supports translation of
-        self.vocab: list[Sign] = vocab
+        self.vocab: list[Sign] = []
+        for i in range(1113):
+            # glosses are all the same order in sessions, organized alphabetically
+            gloss = sessions[0].annotations['lexicon'][i, 0][0]
+            # hand nums are all the same for each session
+            num_hands = 1 if (sessions[0].annotations["type"][i, 0] == 1) else 2
+
+            examples = []
+            for session in sessions:
+                examples.append(SignInfo(i, num_hands, session.handface))
+
+            self.vocab.append(Sign(gloss, num_hands, examples))
 
     def __len__(self):
         """
@@ -15,9 +26,17 @@ class Dictionary:
 
     def __getitem__(self, key):
         """
-        indexing a dictionary returns the ith word
+        indexing a dictionary with an integer returns the ith word
+
+        accessing with a string returns the sign for the gloss
+        this is inefficient as it must search through the whole vocab list
         """
-        return self.vocab[key.gloss]
+        if isinstance(key, int):
+            return self.vocab[key]
+        elif isinstance(key, str):
+            for sign in self.vocab:
+                if sign.gloss == key:
+                    return sign
 
     def word_to_sign(self, word: str):
         """
@@ -25,7 +44,13 @@ class Dictionary:
         """
         raise NotImplementedError
 
-    def sign_to_word(self, top_n, num_hands, unknown: SignInfo):
+    def sign_to_word(self, top_n, num_hands, unknown: SignInfo) -> tuple[list[str], list[str]]:
+        """
+        :param top_n: the top n signs to return
+        :param num_hands: the number of hands in the unknown sign, used to minimize the search space
+        :param unknown: SignInfo object representing the unknown sign
+        :return: (1) the top n signs and (2) the full ranking of all signs in the dictionary
+        """
         predictions = []
         for sign in self.vocab:
             # compare only signs with the same number of hands
@@ -38,7 +63,10 @@ class Dictionary:
         predictions.sort(key=lambda x: x['score'])
         # save just the gloss of predicted signs
         predictions = dict.fromkeys([prediction['sign'] for prediction in predictions])
-        # filter out top n predictions
-        predictions = list(predictions)[:top_n]
 
-        return predictions
+        # full predictions of all 1113 signs
+        full_predictions = list(predictions)
+        # filter out top n predictions
+        top_n_predictions = full_predictions[:top_n]
+
+        return top_n_predictions, full_predictions
